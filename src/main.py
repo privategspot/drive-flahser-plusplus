873698aebda309d15fa0883994b1458b356ec10d
Daniil Frangov <frangov7@gmail.com>
2021-09-28 14:12:23 +0300

    Remove print info about mount root path

--- src/main.py
+++ src/main.py
@@ -5,6 +5,5 @@ if __name__ == "__main__":
     mount_root = "/tmp/flashdrives/"
     mnt = mounter.Mounter(mount_root)
     mnt.mount()
-    print(f"USB флешки были примонтированы в {mount_root}")
     input("Нажмите любую клавишу, чтобы размонитровать подключенные USB устройства")
     mnt.umount()
