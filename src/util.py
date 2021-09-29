276af3907c8c2bc3fc96ca2a7bd1994f495bdc1d
Daniil Frangov <frangov7@gmail.com>
2021-09-29 12:04:57 +0300

    Change the way of path check

--- src/util.py
+++ src/util.py
@@ -1,6 +1,9 @@
+import re
+
+
 def is_path_correct(path):
-    try:
-        with open(path, 'x'):  # OSError if file exists or is invalid
-            return True
-    except OSError:
-        return False
+    pattern = r"^['\"]?(?:/[^/\n]+)*['\"]?$"
+    res = re.match(pattern, path)
+    if res.group(0) is not None:
+        return True
+    return False
